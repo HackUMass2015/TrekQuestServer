3ecc8fb627c94db91a4fa7410f2b749a2979faa9
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-11 19:24:47 -0400

    larger test script

--- user_put_test.sh
+++ user_put_test.sh
@@ -1,1 +1,4 @@
 curl -d '{ "id" : "device-44354", "name" : "danubian" }' -H "Content-Type: application/json" http://52.5.229.43:80/user
+curl -d '{ "id" : "device-23543", "name" : "danubian" }' -H "Content-Type: application/json" http://52.5.229.43:80/user
+curl -d '{ "id" : "device-44354", "name" : "dinubian" }' -H "Content-Type: application/json" http://52.5.229.43:80/user
+curl -d '{ "id" : "device-23543", "name" : "dinubian" }' -H "Content-Type: application/json" http://52.5.229.43:80/user
