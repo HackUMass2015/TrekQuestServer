10b3ca6a249cb0bc602a923424dac012f33f56e4
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-11 18:10:51 -0400

    --adding put at /user

--- db_init.js
+++ db_init.js
@@ -1,69 +1,5 @@
-var fs = require('fs');
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./trek.db');
-var json;
-
-var table_name = "pet_info";
-var pets;
-//console.log(pets);
-var pet;
-var id;
-var name;
-var url;
-
-// fs.readFile('./MockData.json', 'utf8', function (err, data) {
-//     console.log("JSON Initializing...");
-//     if (err) {
-//         console.log("ERROR: JSON load - " + err);
-//         throw err;
-//     } else {
-//         try {
-//             json = JSON.parse(data);
-//             console.log("JSON loaded successfully");
-//             //console.log(json.data);
-
-//             var mockdata = json.mockdata;
-//             //console.log(mockdata);
-//             pets = mockdata.pets
-//             // for (i = 0; i < pets.length; i++)
-//             // {
-//             //   pet = pets[i];
-//             //   id = pet.id;
-//             //   name = pet.name;
-//             //   url = pet.url;
-//             //   console.log(i + ": id: " + id + ", name: " + ", url: " + url);
-//             // }
-
-//             //var pet = data["mockdata"]["pets"][0];
-//             //console.log(pet);
-            
-//             var stmt = db.prepare("INSERT INTO " + table_name + "(name,imageUrl) VALUES (?,?)");
-//             for (var i = 0; i < 10; i++) {
-//                 pet = pets[i];
-//                 id = pet.id;
-//                 name = pet.name;
-//                 url = pet.url;
-//                 console.log("name: " + name + ", url: " + url);
-//                 stmt.run(name, url);
-//                 console.log("Row inserted");
-//             }
-//             stmt.finalize();
-
-//             db.each("SELECT rowid AS id, name, imageUrl FROM " + table_name, function(err, row) {
-//                 console.log(row.id + ": " + row.name + ", " + row.imageUrl);
-//             });
-
-//         }
-//         catch (ex) {
-//             console.log("ERROR: JSON parse - " + err);
-//         }
-//     }
-//     console.log("JSON initialized!");
-
-//     db.close(function() {
-//       console.log("Database Closed");
-//     });
-// });
 
 db.serialize(function() {
   console.log("Database Serialization Initializing...");
@@ -130,3 +66,7 @@ function setupTable(table_name, columns) {
 
 	console.log("Table " + table_name + " initialized!");
 }
+
+db.close(function() {
+      console.log("Database Closed");
+});
--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var app = express();
 var sqlite3	= require('sqlite3').verbose();
+var dbName = './trek.db';
+
+app.use(express.bodyParser());
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.put('/user', function(req, res){
+	console.log(req.body);
+
+	var stmt = db.prepare("INSERT INTO users VALUES (?, ?)");
+	for (var i = 0; i < 10; i++) {
+	  stmt.run("device-" + i, "username " + i);
+	}
+	stmt.finalize();
+
+	db.each("SELECT id, username FROM users", function(err, row) {
+	  console.log(row.id + ": " + row.username);
+	});
+
+	//db.close();
+	res.send(req.body);
 });
 
 var server = app.listen(80, function () {
@@ -12,5 +32,4 @@ var server = app.listen(80, function () {
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
-
 });
