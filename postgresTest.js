542e4a05ae6ecec5ab359f9e2a16b4fa79189b18
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-11 16:24:11 -0400

    --swapping test code

--- postgresTest.js
+++ postgresTest.js
@@ -2,23 +2,20 @@ var pg = require('pg');
 var conString = "postgres://postgres:password@52.5.229.43/mydb";
 
 console.log("Starting test...");
-//this starts initializes a connection pool
-//it will keep idle connections open for a (configurable) 30 seconds
-//and set a limit of 20 (also configurable)
-pg.connect(conString, function(err, client, done) {
+
+var client = new pg.Client(conString);
+client.connect(function(err) {
   console.log("Starting connection...");
   if(err) {
-    return console.error('error fetching client from pool', err);
+    return console.error('could not connect to postgres', err);
   }
-  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
-    //call `done()` to release the client back to the pool
-    done();
-
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
     if(err) {
       return console.error('error running query', err);
     }
-    console.log(result.rows[0].number);
-    //output: 1
+    console.log(result.rows[0].theTime);
+    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+    client.end();
   });
 });
 console.log("Complete");
