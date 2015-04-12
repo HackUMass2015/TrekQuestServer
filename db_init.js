aac4239f803a55c1486d3fa806573c8ac205dbed
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-11 20:00:08 -0400

    primary key maybe?

--- db_init.js
+++ db_init.js
@@ -5,7 +5,7 @@ db.serialize(function() {
   console.log("Database Serialization Initializing...");
 
   //Setting up info tables
-  setupTable("users", "(id TEXT UNIQUE, username TEXT");
+  setupTable("users", "(id TEXT PRIMARY KEY, username TEXT");
   setupTable("teams", "(id TEXT, name TEXT, max INTEGER)");
   setupTable("locs", "(id TEXT, ta_id INTEGER)");
   setupTable("games", "(id TEXT, author_id TEXT, start INTEGER, end INTEGER, points INTEGER)");
