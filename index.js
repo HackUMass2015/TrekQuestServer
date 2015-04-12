ad87bf48ad90241b680a78869eff84ee1a03c0a3
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-12 02:45:02 -0400

    welp

--- index.js
+++ index.js
@@ -111,9 +111,9 @@ function locResponse(){
 //Returns json of all games in a given zip code
 app.get('/localGames', function(req, res){
 	console.log(req.body['zipcode']);
-
+//WHERE games.zipcode = 	" + req.body['zipcode']
 	var json;
-	db.all("SELECT id, end, points, image FROM games INNER JOIN locs ON games.zipcode = locs.zipcode WHERE games.zipcode = " + req.body['zipcode'], function(err, rows) {  
+	db.all("SELECT id, end, points, image FROM games NATURAL JOIN locs", function(err, rows) {  
 		// rows.forEach(function (row) {  
 		//     i++;
 		//     //console.log("Url added to array");
