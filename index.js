47d7a3d930bbc4eb47344cc1ef991ed504a1898c
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-12 00:39:15 -0400

    thsi 'ing

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/loc', function(req, res){
 	request(googleMapsApi + req.body.zipcode, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 			location = body;
-			console.log(location);
+			console.log(location['results']);
 			console.log(Object.getOwnPropertyNames(location));
 			// lat = location.lat;
 			// lng = location.lng;
