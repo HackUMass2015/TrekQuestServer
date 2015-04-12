08604389b61e7ca9b91b7db2769576f239ba8e9f
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-11 20:29:20 -0400

    adding tests

--- loc_put_test.sh
+++ loc_put_test.sh
@@ -2,3 +2,7 @@ curl -d '{ "id" : 12353, "name" : "Eiffel Tower" }' -H "Content-Type: applicatio
 curl -d '{ "id" : 34653, "name" : "Washington Monument" }' -H "Content-Type: application/json" -X PUT http://52.5.229.43:80/user
 curl -d '{ "id" : 12353, "name" : "Hollywood Sign" }' -H "Content-Type: application/json" -X PUT http://52.5.229.43:80/user
 curl -d '{ "id" : 34653, "name" : "Questionable Alley" }' -H "Content-Type: application/json" -X PUT http://52.5.229.43:80/user
+curl -d '{ "id" : 34234, "name" : "Eiffel Tower" }' -H "Content-Type: application/json" -X PUT http://52.5.229.43:80/user
+curl -d '{ "id" : 24345, "name" : "Washington Monument" }' -H "Content-Type: application/json" -X PUT http://52.5.229.43:80/user
+curl -d '{ "id" : 13455, "name" : "Hollywood Sign" }' -H "Content-Type: application/json" -X PUT http://52.5.229.43:80/user
+curl -d '{ "id" : 42544, "name" : "Questionable Alley" }' -H "Content-Type: application/json" -X PUT http://52.5.229.43:80/user
