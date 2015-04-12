9415b74a4dcdfb7b7f4e71932053b3a57c633d48
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-11 20:08:51 -0400

    minuses not pluses

--- user_put_test.sh
+++ user_put_test.sh
@@ -1,4 +1,4 @@
-curl -d '{ "id" : 44354, "name" : "danubian" }' -H "Content-Type: application/json" +X PUT http://52.5.229.43:80/user
-curl -d '{ "id" : 23543, "name" : "danubian" }' -H "Content-Type: application/json" +X PUT http://52.5.229.43:80/user
-curl -d '{ "id" : 44354, "name" : "dinubian" }' -H "Content-Type: application/json" +X PUT http://52.5.229.43:80/user
-curl -d '{ "id" : 23543, "name" : "dinubian" }' -H "Content-Type: application/json" +X PUT http://52.5.229.43:80/user
+curl -d '{ "id" : 44354, "name" : "danubian" }' -H "Content-Type: application/json" -X PUT http://52.5.229.43:80/user
+curl -d '{ "id" : 23543, "name" : "danubian" }' -H "Content-Type: application/json" -X PUT http://52.5.229.43:80/user
+curl -d '{ "id" : 44354, "name" : "dinubian" }' -H "Content-Type: application/json" -X PUT http://52.5.229.43:80/user
+curl -d '{ "id" : 23543, "name" : "dinubian" }' -H "Content-Type: application/json" -X PUT http://52.5.229.43:80/user
